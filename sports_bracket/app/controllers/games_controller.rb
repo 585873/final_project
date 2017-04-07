a9a8b5dea7f0fcba6e44aa74a1e630a7cbf0a969
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-04-07 11:05:10 -0400

    added

--- sports_bracket/app/controllers/games_controller.rb
+++ sports_bracket/app/controllers/games_controller.rb
@@ -7,18 +7,18 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def edit
+    @game = Game.find(params[:id])
+  end
+
   def new
     @game = Game.new
     @league_league = League.all.map{|league| league.name}
   end
- #edit
-  def edit
-    @game = Game.find(params[:id])
-  end
+
  #create
   def create
-    @game = Game.new(game_params.merge(user: current_user))
-    @game.league = league.find_by(name: params[:game][:league])
+    @game = Game.find_by(params[:game])
     @game.save
     redirect_to game_path(@game)
   end
@@ -26,16 +26,12 @@ class GamesController < ApplicationController
   def update
    @game = Game.find(params[:id])
    @game.update(game_params)
-   redirect_to game_path(@game)
+   redirect_to game_path
  end
  #delete
  def destroy
    @game = Game.find(params[:id])
-  # if @student.instructor == current_user
     @game.destroy
-  # else
-  #   flash[:alert] = "Only the original author can delete"
-  # end
   redirect_to games_path
 end
 
